857b467dd61f0dba6ff563ebeb8b8ce6288dc5d6
ssheshad <siddharth.sheshadri@optum.com>
2021-02-08 08:59:28 -0600

    Day 4

--- python_files/day4.py
+++ python_files/day4.py
@@ -1,23 +1,22 @@
 import hashlib 
 
-#Part 1
-i=0
-key = 'yzbqklnj'
-result=''
-while(True):
-    result = hashlib.md5((key+str(i)).encode()).hexdigest()
-    if result[:5]=='00000':
-        break
-    i=i+1
-
-#Part 2
-i=0
-key = 'yzbqklnj'
-result=''
-while(True):
-    result = hashlib.md5((key+str(i)).encode()).hexdigest()
-    if result[:6]=='000000':
-        break
-    i=i+1
+def hash_key_val(s, val):
+    i=0
+    result=''
+    zero_str = '0'*val
+    while(True):
+        result = hashlib.md5((s+str(i)).encode()).hexdigest()
+        if result[:val]==zero_str:
+            break
+        i=i+1
+    return i
     
-print('Value for 6 zeros = ', i)
+    
+def main():
+    #s = 'yzbqklnj'
+    s = input('Enter string here (with ""): ')
+    print('Part 1', hash_key_val(s, 5))
+    print('Part 2', hash_key_val(s, 6))
+    
+if __name__ == "__main__":
+    main()

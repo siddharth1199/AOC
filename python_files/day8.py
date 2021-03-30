50e1989afaec06e7545bcdb00eeb63cea318076b
ssheshad <siddharth.sheshadri@optum.com>
2021-03-30 05:34:15 -0500

    Day 8

---
+++ python_files/day8.py
@@ -0,0 +1,22 @@
+import sys
+
+def load_file(path):
+    with open(path, "r") as f:
+        lines = f.read().split("\n")
+        return lines
+    
+def main(path):
+    str_len = raw_len = encoded_len = 0
+    lines =  load_file(path)
+    for l in lines:
+        str_len += len(eval(l))
+        raw_len += len(l)
+        encoded_len += 2+l.count('\\')+l.count('"')+len(l)
+    print('Part 1 = ', (raw_len - str_len))
+    print('Part 2 = ', (encoded_len - raw_len))
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("USAGE: python [script.py] [input.txt]")
+    else:
+        main(sys.argv[1])

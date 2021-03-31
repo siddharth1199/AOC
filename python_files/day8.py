8f6caa042a1bf3b4794069841639b9fdbd54196a
ssheshad <siddharth.sheshadri@optum.com>
2021-03-31 06:05:33 -0500

    Logic explained

--- python_files/day8.py
+++ python_files/day8.py
@@ -1,3 +1,5 @@
+# SAY NO TO REGEX!!!
+
 import sys
 
 def load_file(path):
@@ -9,10 +11,14 @@ def main(path):
     str_len = raw_len = encoded_len = 0
     lines =  load_file(path)
     for l in lines:
+        # Part 1
+        # Eval does the trick of parsing the escape characters 
         str_len += len(eval(l))
         raw_len += len(l)
         # Part 2
         # Basically, count each time a \ (rep by \\) and " appears and add it to the complete length
+        # This is done because encoding any of the 2 chars results in 1 extra char,
+        # no matter what, we just need length and not if it is \(+1\) or (+1\)"
         # Also add 2 to acount for the 2 " in the beginning and end
         encoded_len += 2+l.count('\\')+l.count('"')+len(l) 
     print('Part 1 = ', (raw_len - str_len))

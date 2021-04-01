70e4cccb1e5f82cea2684fa8572520d1be457449
siddharth1199 <siddharth1199@gmail.com>
2021-04-01 15:44:07 +0100

    Update day8.py

--- python_files/day8.py
+++ python_files/day8.py
@@ -4,15 +4,16 @@ import sys
 
 def load_file(path):
     with open(path, "r") as f:
-        lines = f.read().split("\n")
-        return lines
-    
+        lines = f.readlines()
+        return [i.strip() for i in lines]
+
+
 def main(path):
     str_len = raw_len = encoded_len = 0
-    lines =  load_file(path)
+    lines = load_file(path)
     for l in lines:
         # Part 1
-        # Eval parses the escape characters 
+        # Eval parses the escape characters
         str_len += len(eval(l))
         raw_len += len(l)
         # Part 2
@@ -20,9 +21,10 @@ def main(path):
         # This is done because encoding any of the 2 chars results in 1 extra char,
         # no matter what, we just need length and not if it is \(+1\) or (+1\)"
         # Also add 2 to acount for the 2 " in the beginning and end
-        encoded_len += 2+l.count('\\')+l.count('"')+len(l) 
+        encoded_len += 2 + l.count('\\') + l.count('"') + len(l)
     print('Part 1 = ', (raw_len - str_len))
     print('Part 2 = ', (encoded_len - raw_len))
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
